18b1122cf7930fe3eb0b27fe3cba50e5b64130c0
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-15 12:48:31 -0500

    fixed testing suite

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -3,6 +3,6 @@ const Engineer = require('../lib/Engineer.js');
 test('returns engineer github', () => {
     const emp = new Engineer('Dave', 1,'dave@company.com','github');
   
-    expect(emp.getGithubURL()).toBe(`github`)
+    expect(emp.getGithub()).toBe(`github`)
     
   });
--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -3,7 +3,7 @@ const Intern = require('../lib/Intern.js');
 test('returns interns school', () => {
     const emp = new Intern('Dave', 1,'dave@company.com','school');
   
-    expect(emp.getInternSchool()).toEqual('school');
+    expect(emp.getSchool()).toEqual('school');
   
     
   }); 
