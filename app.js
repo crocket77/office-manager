fb1d285f0cc2b65c2f9e194e0bd906b4ed8737f2
crocket77 <ethanpfeiffer345@outlook.com>
2022-04-15 09:54:48 -0500

    refactored inquirer and made it function properly so you can add as many engineers and interns as you want

--- app.js
+++ app.js
@@ -2,246 +2,134 @@ const inquirer = require('inquirer');
 const Intern = require('./lib/Intern');
 const Engineer = require('./lib/Engineer');
 const Employee = require('./lib/Employee.js');
+const Manager = require('./lib/Manager')
 const generatePage = require('./src/page-template.js');
-//const { writeFile, copyFile } = require('./utils/generate-site');
-
+const { writeFile, copyFile } = require('./utils/generate-site');
+let engineerArr = [];
+let internArr = [];
   
-class Office {
-  constructor() {
-    this.manager;
-    this.mEmail;
-    this.mId;
-    this.isEngineers;
-    this.isInterns;
-    this.officeID
-    this.numEmployees=1;
-    this.employeeArr = [];
 
-  }
 
-initializeOffice() {
-
-    let mName, mId, mEmail,officeID;
-    inquirer
-      .prompt({
+async function initManager() {
+    return inquirer
+    .prompt([
+      {
         type: 'text',
         name: 'name',
         message: 'Who is the Manager?'
-      })
-      .then(({ name }) => {
-        mName = name;
-        inquirer
-        .prompt({
-            type: 'text',
-            name: 'managerID',
-            message: 'What is the Managers ID?'
-        })
-        .then(({ managerID }) => {
-            mId = managerID;
-            console.log(`managerID ${mId}`)
-            
-            inquirer
-            .prompt({
-            type: 'text',
-            name: 'managerEmail',
-            message: 'What is the Managers email?'
-            })
-            .then(({ managerEmail }) => {
-            mEmail = managerEmail;
-            console.log(`managerEmail ${mEmail}`)
-            
-            inquirer
-            .prompt({
-            type: 'text',
-            name: 'officeNumber',
-            message: 'What is the Office Id?'
-            })
-            .then(({ officeNumber }) => {
-            officeID = officeNumber;
-            console.log(`office ID ${officeID}`)
-            this.manager= new Employee(mName,mId, mEmail,'manager')
-            this.employeeArr.push(this.manager)
-            console.log(`manager object ${this.manager.role}`)
-
-            this.menuChoice();
-            
-
-    }); }); }); }); 
+      },
+      {
+        type: 'text',
+        name: 'managerID',
+        message: 'What is the Managers ID?'
+      },
+      {
+        type: 'text',
+        name: 'managerEmail',
+        message: 'What is the Managers email?'
+      },
+      {
+        type: 'text',
+        name: 'officeNumber',
+        message: 'What is the Office Id?'
+      }
+    ]).then(mData=>{
+         return new Manager(mData.name, mData.managerID, mData.managerEmail, mData.officeNumber);
+    })
 }
-menuChoice(){
-    inquirer
+
+
+
+async function menuChoice(){
+    return inquirer
     .prompt({
-    type: 'list',
-    name: 'choice',
-    message: 'What would you like to do?',
-    choices: ["Add an engineer", "Add an intern", "Finish Team"]
-    })
-    .then(({ choice }) => {
-    if(choice==="Add an engineer"){
-        console.log('engineer chosen')
-        this.addEngineer("engineer");
-    }
-    else if(choice==="Add an intern"){
-        this.addIntern("intern");
-    }
-    else{
-        this.generateReport();
-        for(let i=0; i<this.employeeArr.length;i++){
-            console.log(this.employeeArr[i].name)
-        }
-    } 
-    })
-}
-
-
-  addEngineer(empRole) {
+      type: 'list',
+      name: 'choice',
+      message: 'What would you like to do?',
+      choices: ["Add an engineer", "Add an intern", "Finish Team"]
+    }).then(data=>{;
+      if (data.choice === "Add an engineer") {
+        return inquirer
+        .prompt([
+          {
+            type: 'text',
+            name: 'name',
+            message: "What is the engineers's name?",
+          },
+          {
+            type: 'text',
+            name: 'id',
+            message: "What is the engineers's id?"
+          },
+          {
+            type: 'text',
+            name: 'email',
+            message: "What is the engineers's email?"
+          },
+          {
+            type: 'text',
+            name: 'github',
+            message: "What is the engineers's github?"
+          }
+        ]).then(data=>{engineerArr.push(new Engineer(data.name, data.id, data.email, data.github));})
+        .then(()=>{
+           console.log(engineerArr)
+           return menuChoice();
+      })
         
-        inquirer
-        .prompt({
-        type: 'text',
-        name: 'eName',
-        message: 'What is the engineers name?'
+        
+      }else if (data.choice === "Add an intern") {
+        return inquirer
+        .prompt([
+          {
+            type: 'text',
+            name: 'name',
+            message: "What is the intern's name?",
+          },
+          {
+            type: 'text',
+            name: 'id',
+            message: "What is the intern's id?"
+          },
+          {
+            type: 'text',
+            name: 'email',
+            message: "What is the intern's email?"
+          },
+          {
+            type: 'text',
+            name: 'school',
+            message: "What school did the intern go to?"
+          }
+        ]).then(data=>{
+             internArr.push(new Intern(data.name, data.id, data.email, data.school));
+        }).then(()=>{
+              console.log(internArr)
+              return menuChoice();
         })
-        .then(({ eName }) => {
-            console.log(`engineer name: ${eName}`)
-            
-            inquirer
-            .prompt({
-                type: 'text',
-                name: 'eID',
-                message: 'What is the engineers employee ID?'
-                })
-                .then(({ eID }) => {
-                    console.log(`engineer id: ${eID}`)
-                    
-                    inquirer
-                    .prompt({
-                        type: 'text',
-                        name: 'eEmail',
-                        message: 'What is the engineers email?'
-                        })
-                        .then(({ eEmail }) => {
-                            console.log(`engineer email: ${eEmail}`)
-                           
-                            inquirer
-                            .prompt({
-                                type: 'text',
-                                name: 'github',
-                                message: 'What is the engineers github account?'
-                                })
-                                .then(({ github}) => {
-                                    console.log(`engineer name: ${eEmail}`)
-                                    const engineer = new Engineer(eName, eID, eEmail,empRole,github)
-                                    this.employeeArr.push(engineer)
-
-                                    this.menuChoice();
-        });});});});
-   
-    }
-
-    addIntern(impRole) {
-        let iName, iID, iEmail, school;
-        inquirer
-        .prompt({
-        type: 'text',
-        name: 'iName',
-        message: 'What is the interns name?'
-        })
-        .then(({ iName }) => {
-            console.log(`interns name: ${iName}`)
-            
-            inquirer
-            .prompt({
-                type: 'text',
-                name: 'iID',
-                message: 'What is the interns employee ID?'
-                })
-                .then(({ iID }) => {
-                    console.log(`intern id: ${iID}`)
-                    
-                    inquirer
-                    .prompt({
-                        type: 'text',
-                        name: 'iEmail',
-                        message: 'What is the interns email?'
-                        })
-                        .then(({ iEmail }) => {
-                            console.log(`intern email: ${iEmail}`)
-                           
-                            inquirer
-                            .prompt({
-                                type: 'text',
-                                name: 'school',
-                                message: 'What is the interns school?'
-                                })
-                                .then(({ github}) => {
-                                    console.log(`intern school: ${school}`)
-                                    const intern = new Intern(iName, iID, iEmail,impRole,school)
-                                    this.employeeArr.push(intern)
-
-                                    this.menuChoice();
-        });});});});
-   
-    }
-//figure this out
-    generateReport(){
-        console.log("figure out how to generate html report")
-        generatePage(this.employeeArr);
-        generatePage(office.sportsArr)
-
-        .then(pageHTML => {
-        return writeFile(pageHTML);
-        })
-        .then(writeFileResponse => {
-        console.log(writeFileResponse);
-        return copyFile();
-        })
-        .then(copyFileResponse => {
-        console.log(copyFileResponse);
-        })
-        .catch(err => {
-        console.log(err);
-        });
-
-    }
-
-}
-  
-const writeFile = fileContent => {
-  return new Promise((resolve, reject) => {
-    fs.writeFile('./dist/index.html', fileContent, err => {
-      if (err) {
-        reject(err);
+        
+      }
+      else {
         return;
       }
+  }) 
+}
 
-      resolve({
-        ok: true,
-        message: 'File created!'
-      });
-    });
-  });
-};
-getInfo()
-  .then(data => {
-      getEmp()
-          .then(() => {
-              writeFile(html(data, engArr, intArr));
-          })
+
+
+
+initManager()
+  .then(manager=>{
+    menuChoice()
+     .then(()=>{
+      copyFile()
+      writeFile(generatePage(manager,engineerArr,internArr)).then(()=>{
+    })
   })
+})
+
+  
 
 
-// generatePage(office.sportsArr)
-// .then(pageHTML => {
-//   return writeFile(pageHTML);
-// })
-// .then(writeFileResponse => {
-//   console.log(writeFileResponse);
-//   return copyFile();
-// })
-// .then(copyFileResponse => {
-//   console.log(copyFileResponse);
-// })
-// .catch(err => {
-//   console.log(err);
-// });
+
+
